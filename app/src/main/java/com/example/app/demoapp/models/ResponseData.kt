a23bdcce61b90158c4ca7ecdeedb62ed5bd71e5c
shweta tiwari <shweta.tiwari@shipsy.co.in>
2020-02-26 00:41:23 +0530

    initial commit

---
+++ app/src/main/java/com/example/app/demoapp/models/ResponseData.kt
@@ -0,0 +1,3 @@
+package com.example.app.demoapp.models
+
+data class ResponseData(@SerializedName(""))
---
+++ app/src/main/java/com/example/app/demoapp/views/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.app.demoapp.views
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.app.demoapp.R
+
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
